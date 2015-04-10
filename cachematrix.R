fa37a1decc065ff0ee54c291eac4f8cdc2000782
Alexander Connelly <connella@colorado.edu>
2015-04-10 17:10:31 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix takes a matrix and runs an inversion on it using solve(matrix)
+## this will automatically cache the result so it only needs to
+##run the inversion once
 
-## Write a short comment describing this function
+## makeCacheMatrix will create the matrix for inversing,
+##thiss where the CasheMatrix will store the precomputed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+     setmatrix=setmatrix,
+     getmatrix=getmatrix)
 }
 
+## cacheSolve will run on the marix from makeCacheMatrix
+##but will also check to see if the matrix has alreddy ran or not
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        ## Return a matrix that is the inverse of 'x' 
+        ##if the value of the matrix is null
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
+
